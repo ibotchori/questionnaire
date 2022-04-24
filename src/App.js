c10202d88935c4d406d74d69578f789877cf9e9d
Irakli <i.botchori@gmail.com>
2022-04-24 17:25:59 +0400

    Add Landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Landing.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Logo from "../assets/images/main-logo.png";
+
+const Landing = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-200 ">
+      <Link to="/identification">
+        <img alt="Logo" className="w-24 h-24 mb-20 " src={Logo} />
+      </Link>
+      <Link to="/identification">
+        <h1 className="text-3xl w-48 text-center font-bold">
+          კითხვარის დაწყება
+        </h1>
+      </Link>
+    </div>
+  );
+};
+
+export default Landing;
