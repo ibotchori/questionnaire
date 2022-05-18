b70aa0fec562409e2a81b75407b8c94a55616342
Irakli <i.botchori@gmail.com>
2022-05-18 23:41:53 +0400

    feat: change page number dynamically on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
+  const [pageNumber, setPageNumber] = useState("");
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/identification":
+        setPageNumber("1/4");
+        break;
+      case "/covid":
+        setPageNumber("2/4");
+        break;
+      case "/injection":
+        setPageNumber("3/4");
+        break;
+      case "/advice":
+        setPageNumber("4/4");
+        break;
+    }
+  }, [location.pathname]);
+
   return (
     <div className="px-5 sm:px-20 md:px-40 pt-10  ">
       <div className="flex justify-between w-full border-b-2 pb-2 border-gray-500 ">
         <p className="text-red-500 font-bold text-lg">REDBERRY</p>
-        <div className="font-bold">1/4</div>
+        <div className="font-bold">{pageNumber}</div>
       </div>
     </div>
   );
