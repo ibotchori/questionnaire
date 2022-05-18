83f59795d58a4b70ca36206939ec7130524fcdc7
Irakli <i.botchori@gmail.com>
2022-05-18 18:03:59 +0400

    style: media query on Thanka page

--- src/pages/Thanks.js
+++ src/pages/Thanks.js
@@ -3,7 +3,7 @@ import React from "react";
 const Thanks = () => {
   return (
     <div className="flex flex-col items-center justify-center  h-screen bg-black ">
-      <h1 className="text-6xl tracking-[.5em] w-48 mr-72 text-white font-bold">
+      <h1 className="lg:text-6xl tracking-[.5em] md:text-5xl sm:text-4xl text-3xl   max-w-48 text-white font-bold">
         მადლობა
       </h1>
     </div>
