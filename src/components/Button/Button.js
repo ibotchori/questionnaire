8d46c685d87c6f3537336f45ce26424a37124851
Irakli <i.botchori@gmail.com>
2022-05-19 16:23:49 +0400

    feat: add navigate to Thanks page on button click

---
+++ src/components/Button/Button.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Button = ({ text }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/thanks");
+  };
+  return (
+    <button
+      onClick={handleClick}
+      className="bg-button-100 hover:bg-button-200 text-white font-bold py-3 px-6 rounded-full w-40 "
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
--- src/pages/Thanks.js
+++ src/pages/Thanks.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Thanks = () => {
+  const navigate = useNavigate();
+
+  setTimeout(() => {
+    navigate("/");
+  }, 3000);
+
   return (
     <div className="flex flex-col items-center justify-center  h-screen bg-black ">
       <h1 className="lg:text-6xl tracking-[.5em] md:text-5xl sm:text-4xl text-3xl   max-w-48 text-white font-bold">
