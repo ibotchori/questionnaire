5cf5a5e8a56bfb47584ca50d9c0b084cf3e5502f
Irakli <i.botchori@gmail.com>
2022-05-19 12:19:23 +0400

    style: change width to max-width on Info component

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -3,7 +3,7 @@ import React from "react";
 const Info = ({ text, icon, header }) => {
   return (
     <div className="pl-5 pb-4">
-      {header && <p className="w-96 pb-4">{header}</p>}
+      {header && <p className="max-w-sm pb-4">{header}</p>}
       {text && <p>{text}</p>}
       <a href="https://booking.moh.gov.ge/" className="text-blue-500">
         <p></p>
