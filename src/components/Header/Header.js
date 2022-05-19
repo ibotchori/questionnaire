33bed10ade2b57e543551123811ec001261ba5bb
Irakli <i.botchori@gmail.com>
2022-05-19 19:12:43 +0400

    style: increase font weight on Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,9 +23,9 @@ const Header = () => {
 
   return (
     <div className="px-5 sm:px-20 md:px-40 pt-10  ">
-      <div className="flex justify-between w-full border-b-2 pb-2 border-gray-500 ">
-        <p className="text-red-500 font-bold text-lg">REDBERRY</p>
-        <div className="font-bold">{pageNumber}</div>
+      <div className="flex justify-between w-full text-2xl border-b-2 pb-2 border-gray-500 ">
+        <p className="text-red-500 font-black">REDBERRY</p>
+        <div className="font-semibold tracking-widest">{pageNumber}</div>
       </div>
     </div>
   );
