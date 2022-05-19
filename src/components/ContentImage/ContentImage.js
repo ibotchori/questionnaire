0557150fcd93638582e51a16270897812b0d6e65
Irakli <i.botchori@gmail.com>
2022-05-19 19:05:04 +0400

    style: fix overflow problem on ContentImage

--- src/components/ContentImage/ContentImage.js
+++ src/components/ContentImage/ContentImage.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const ContentImage = ({ src }) => {
   return (
-    <div className="max-w-3xl max-h-96   hidden lg:block ">
-      <img className="" alt="" src={src} />
+    <div className=" hidden lg:block">
+      <img className=" max-h-[670px]  mr-[150px]  " alt="" src={src} />
     </div>
   );
 };
