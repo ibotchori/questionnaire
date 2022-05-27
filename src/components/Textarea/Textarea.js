022fbb7070755b695b8548340aa77fe2fdbe9be9
Irakli <i.botchori@gmail.com>
2022-05-27 17:14:02 +0400

    reafactor: add props on TextArea component

--- src/components/Textarea/Textarea.js
+++ src/components/Textarea/Textarea.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Textarea = ({ title, name }) => {
+const Textarea = ({ title, name, register, value }) => {
   return (
     <div className="max-w-lg">
       <p className="pt-6 pb-4 font-bold ">{title}</p>
@@ -10,6 +10,8 @@ const Textarea = ({ title, name }) => {
         rows="4"
         cols="40"
         className="  rounded-lg border-transparent flex-1 appearance-none border border-gray-400 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-transparent"
+        {...register(name)}
+        defaultValue={value}
       ></textarea>
     </div>
   );
