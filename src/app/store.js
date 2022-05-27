5ded95bb8b676d6e9e6fcb86f65149bfb5611e3e
Irakli <i.botchori@gmail.com>
2022-05-27 16:26:04 +0400

    feat: add advice reducer in store

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import identificationReducer from "../features/identification/identificationSlice";
 import covidReducer from "../features/covid/covidSlice";
 import injectionReducer from "../features/injection/injectionSlice";
+import adviceReducer from "../features/advice/adviceSlice";
 
 export const store = configureStore({
   reducer: {
     identification: identificationReducer,
     covid: covidReducer,
     injection: injectionReducer,
+    advice: adviceReducer,
   },
 });
