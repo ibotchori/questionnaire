f32c99985e6e0612961b017dddbf05faf8762c19
Irakli <i.botchori@gmail.com>
2022-05-31 23:30:55 +0400

    wip: chnage stucture of image imports

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+export const contentImages = {
+  identificationImage: require("../images/"),
+  covidImage: require("./pins/stroked/fast-private-free.png"),
+  injectionImage: require("./pins/stroked/fast-private-not-working.png"),
+  adviceImage: require("./pins/stroked/fast-private.png"),
+};
