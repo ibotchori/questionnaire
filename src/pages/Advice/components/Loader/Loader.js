57d632523dff425f1d50547be1cf38978dded98b
Irakli <i.botchori@gmail.com>
2022-06-06 00:44:19 +0400

    style: change class to className

--- src/pages/Advice/components/Loader/Loader.js
+++ src/pages/Advice/components/Loader/Loader.js
@@ -5,7 +5,7 @@ const Loader = () => {
     <div className="flex justify-center h-full w-full fixed items-center">
       <svg
         role="status"
-        class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#FF6A3D]"
+        className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#FF6A3D]"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
