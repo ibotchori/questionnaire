6e935220e97fd930f5656be02104806d93486999
Irakli <i.botchori@gmail.com>
2022-06-05 13:08:05 +0400

    style: edit structure of html elements in ErroPage

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -15,13 +15,16 @@ const ErrorPage = () => {
         დაფიქსირდა შეცდომა
       </div>
       <button className="mt-5">
-        <a className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
+        <Link
+          to="/"
+          className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
+        >
           <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
           <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-            <Link to="/">მთავარი გვერდი</Link>
+            <div>მთავარი გვერდი</div>
           </span>
-        </a>
+        </Link>
       </button>
     </main>
   );
